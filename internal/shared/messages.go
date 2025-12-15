c34b8c4d5c11034e4f2fd9ce3e002683e47dabce
m-mdy-m <bitsgenix@gmail.com>
2025-12-16 02:32:22 +0330

    refactor(message): fix cycle import

--- internal/shared/messages.go
+++ internal/shared/messages.go
@@ -2,8 +2,6 @@ package shared
 
 import (
 	"fmt"
-
-	"github.com/m-mdy-m/psx/internal/resources"
 )
 
 // Exit codes
@@ -14,21 +12,6 @@ const (
 	ExitFS      = 3
 	ExitArgs    = 4
 )
-
-// Exit status messages
-func ExitMessage(code int) string {
-	codeMap := map[int]string{
-		ExitSuccess: "success",
-		ExitFailed:  "failed",
-		ExitConfig:  "config",
-		ExitFS:      "fs",
-		ExitArgs:    "args",
-	}
-	if key, ok := codeMap[code]; ok {
-		return resources.ExitMessage(key)
-	}
-	return "Unknown error"
-}
 
 // Error messages
 const (
@@ -41,70 +24,10 @@ const (
 	ErrDetectionFailed  = "detection_failed"
 )
 
-func ErrorMessage(errType string) string {
-	return resources.ErrorMessage(errType)
-}
-
-// Help messages
-func Help() {
-	fmt.Print(resources.HelpMain())
-}
-
 func Version(version string) {
 	fmt.Printf("PSX version %s\n", version)
 }
 
 func VerboseVersion(version string) {
 	fmt.Printf("PSX version %s\nLicense: MIT\nRepository: https://github.com/m-mdy-m/psx\n", version)
-}
-
-// Success messages
-func CheckSuccess(passed, total int) string {
-	return resources.CheckSuccess(passed, total)
-}
-
-func FixSuccess(fixed int) string {
-	return resources.FixSuccess(fixed)
-}
-
-func InitSuccess(path string) string {
-	return resources.InitSuccess(path)
-}
-
-// Verbose messages
-func VerboseCheckStart(path string) string {
-	return resources.CheckStart(path)
-}
-
-func VerboseDetected(projectType string) string {
-	return resources.VerboseDetected(projectType)
-}
-
-func VerboseRulesLoaded(count int) string {
-	return resources.VerboseRulesLoaded(count)
-}
-
-func VerboseRuleCheck(ruleID string) string {
-	return resources.CheckRule(ruleID)
-}
-
-func VerboseFixApplied(ruleID string) string {
-	return resources.FixApplied(ruleID)
-}
-
-func VerboseConfigLoaded(path string) string {
-	return resources.VerboseConfigLoaded(path)
-}
-
-// Fix messages
-func FixPrompt(count int) string {
-	return resources.FixPrompt(count)
-}
-
-func FixDryRunHeader() string {
-	return resources.FixDryRun()
-}
-
-func FixInteractiveHeader() string {
-	return resources.FixInteractive()
 }
